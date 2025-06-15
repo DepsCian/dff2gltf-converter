0e83aa695faa1cbe2d0d7777bb500ec5929fb4a1
Anders <morgenstern089@gmail.com>
2025-06-15 10:05:45 +0500

    Add error handling

--- converter.ts
+++ converter.ts
@@ -1,96 +1,128 @@
 import { Document, Primitive, PropertyType } from '@gltf-transform/core';
-import { DffParser, RwTextureCoordinate, RwTxd, TxdParser } from 'rw-parser';
+import { DffParser, RwBinMesh, RwMesh, RwTextureCoordinate, RwTxd, RwVector3, TxdParser } from 'rw-parser';
 import { PNG } from 'pngjs';
 import { normals as normalize, dedup } from '@gltf-transform/functions';
+import { error } from 'console';
 
 
 export default async function convertDffToGlb( dff: Buffer, txd: Buffer): Promise<Document> {
 
   const doc = new Document();
   const buffer = doc.createBuffer();
-
-   /// TEXTURES
-  const rwTxd: RwTxd = new TxdParser(txd).parse();
   const texturesMap: Map<String, Buffer> = new Map();
 
-  for (const textureNative of rwTxd.textureDictionary.textureNatives) {
-    const pngBuffer = await createPNGBufferFromRGBA(Buffer.from(textureNative.mipmaps[0]), textureNative.width, textureNative.height);
-    texturesMap.set(textureNative.textureName, pngBuffer);
+   /// TEXTURES
+  try {
+    const rwTxd: RwTxd = new TxdParser(txd).parse();
+
+    if (rwTxd.textureDictionary.textureCount < 1) {
+      throw new Error;
+    }
+    for (const textureNative of rwTxd.textureDictionary.textureNatives) {
+      const pngBuffer = await createPNGBufferFromRGBA(Buffer.from(textureNative.mipmaps[0]), textureNative.width, textureNative.height);
+      texturesMap.set(textureNative.textureName, pngBuffer);
+    }
+  } catch(e) {
+    console.error('Invalid .txd file.');
+    return null;
   }
 
 
   // GEOMETRIES
-  const rwDff = new DffParser(dff).parse();
-
-  for (const rwGeometry of rwDff.geometryList.geometries) {
-    const mesh = doc.createMesh();
-    const {
-      vertexInformation: rwVerticesArray,
-      textureMappingInformation: rwTextureInfo,
-      normalInformation: rwNormalsArray,
-      binMesh: rwBinMesh,
-    } = rwGeometry;
-    const rwUvsArray: RwTextureCoordinate[] = rwTextureInfo && rwTextureInfo.length > 0 ? rwTextureInfo[0] : undefined;
+  try {
+    const rwDff = new DffParser(dff).parse();
   
-    const vertices = new Float32Array(rwVerticesArray.length * 3);
-    rwVerticesArray.forEach((vertex, i) => {
-      vertices[i * 3] = vertex.x;
-      vertices[i * 3 + 1] = vertex.y;
-      vertices[i * 3 + 2] = vertex.z;
-    });
+    for (const rwGeometry of rwDff.geometryList.geometries) {
+      const mesh = doc.createMesh();
   
-    const uvs = new Float32Array(rwUvsArray.length * 2);
-    rwUvsArray.forEach((uv, i) => {
-      uvs[i * 2] = uv.u;
-      uvs[i * 2 + 1] = uv.v;
-    });
+      const rwTextureInfo = rwGeometry.textureMappingInformation;
+      const rwUvsArray :RwTextureCoordinate[] = rwTextureInfo && rwTextureInfo.length > 0 ? rwTextureInfo[0] : undefined;
+      const rwVerticesArray :RwVector3[] = rwGeometry.hasVertices && rwGeometry.vertexInformation.length > 0 ? rwGeometry.vertexInformation : undefined;
+      const rwNormalsArray :RwVector3[] = rwGeometry.hasNormals && rwGeometry.normalInformation.length > 0 ? rwGeometry.normalInformation : undefined;
+      const rwBinMesh :RwBinMesh = rwGeometry.binMesh && rwGeometry.binMesh.meshes.length > 0 ? rwGeometry.binMesh : undefined;
   
-    const normals = new Float32Array(rwNormalsArray.length * 3);
-    rwNormalsArray.forEach((normal, i) => {
-      normals[i * 3] = normal.x;
-      normals[i * 3 + 1] = normal.y;
-      normals[i * 3 + 2] = normal.z;
-    });
+      if (rwTextureInfo == undefined || rwUvsArray == undefined || rwVerticesArray == undefined || rwNormalsArray == undefined || rwBinMesh == undefined) {
+        throw new Error(`Invalid .dff file.`);
+      }
+    
+      const vertices = new Float32Array(rwVerticesArray.length * 3);
+      rwVerticesArray.forEach((vertex, i) => {
+        vertices[i * 3] = vertex.x;
+        vertices[i * 3 + 1] = vertex.y;
+        vertices[i * 3 + 2] = vertex.z;
+      });
+    
+      const uvs = new Float32Array(rwUvsArray.length * 2);
+      rwUvsArray.forEach((uv, i) => {
+        uvs[i * 2] = uv.u;
+        uvs[i * 2 + 1] = uv.v;
+      });
+    
+      const normals = new Float32Array(rwNormalsArray.length * 3);
+      rwNormalsArray.forEach((normal, i) => {
+        normals[i * 3] = normal.x;
+        normals[i * 3 + 1] = normal.y;
+        normals[i * 3 + 2] = normal.z;
+      });
+  
+      const positionsAccessor = doc.createAccessor().setType("VEC3").setArray(vertices);
+      const normalsAccessor = doc.createAccessor().setType("VEC3").setArray(normals);
+  
+      for (const rwPrimitive of rwBinMesh.meshes) {
+        const indices = new Uint32Array(rwPrimitive.indices);
+        const materialIndex = rwPrimitive.materialIndex;
+        const rwMaterial = rwGeometry.materialList.materialData[materialIndex];
 
-    const positionsAccessor = doc.createAccessor().setType("VEC3").setArray(vertices);
-    const normalsAccessor = doc.createAccessor().setType("VEC3").setArray(normals);
+        const material = doc.createMaterial(`${materialIndex}_Mtl`)
+        .setBaseColorFactor([1, 1, 1, 1])
+        .setMetallicFactor(0)
+        .setRoughnessFactor(1);
 
-    for (const rwPrimitive of rwBinMesh!.meshes) {
-      const indices = new Uint32Array(rwPrimitive.indices);
-      const materialIndex = rwPrimitive.materialIndex;
-      const textureName =   rwDff.geometryList.geometries[0].materialList.materialData[materialIndex].texture!.textureName;
-      const pngBuffer :Buffer = texturesMap.get(textureName);
+        let texture = undefined;
 
-      const texture = doc.createTexture()
-        .setImage(pngBuffer)
-        .setMimeType("image/png")
-        .setName(textureName);
+        if (rwMaterial.isTextured) {
+          const textureName = rwMaterial.texture.textureName;
+          const pngBuffer :Buffer = texturesMap.get(textureName);
+    
+          texture = doc.createTexture()
+            .setImage(pngBuffer)
+            .setMimeType("image/png")
+            .setName(textureName);
 
-      const primitive = doc.createPrimitive() 
-        .setMode(Primitive.Mode.TRIANGLES)
-        .setAttribute("POSITION", positionsAccessor)
-            .setMaterial(doc.createMaterial(`${textureName}Mtl`)
-              .setBaseColorTexture(texture)
-              .setBaseColorFactor([1, 1, 1, 1])
-              .setMetallicFactor(0)
-              .setRoughnessFactor(1))
-        .setAttribute("TEXCOORD_0", doc.createAccessor()
-            .setType("VEC2")
-            .setArray(uvs))
-        .setIndices(doc.createAccessor()
-            .setType("SCALAR")
-            .setArray(indices))
-        .setAttribute("NORMAL", normalsAccessor);
-        
-    mesh.addPrimitive(primitive);
+          material.setBaseColorTexture(texture);
 
+        } else {
+
+
+        }
+  
+        const primitive = doc.createPrimitive() 
+          .setMode(Primitive.Mode.TRIANGLES)
+          .setAttribute("POSITION", positionsAccessor)
+              .setMaterial(material)
+          .setAttribute("TEXCOORD_0", doc.createAccessor()
+              .setType("VEC2")
+              .setArray(uvs))
+          .setIndices(doc.createAccessor()
+              .setType("SCALAR")
+              .setArray(indices))
+          .setAttribute("NORMAL", normalsAccessor);
+          
+      mesh.addPrimitive(primitive);
+  
+      }
+      const node = doc.createNode().setMesh(mesh);
+      const scene = doc.createScene().addChild(node);
     }
-    const node = doc.createNode().setMesh(mesh);
-    const scene = doc.createScene().addChild(node);
+  } catch(e) {
+    console.error(`${e} Cannot create geometry mesh.`)
+    return null;
   }
 
+
+  // POST-PROCESSING
   await doc.transform(normalize());
-  await doc.transform(dedup({propertyTypes: [PropertyType.MESH, PropertyType.ACCESSOR]}));
+ // await doc.transform(dedup({propertyTypes: [PropertyType.MESH, PropertyType.ACCESSOR]}));
 
   return doc;
 }
